a1a8d118a82bc0785d54ef01735e24021a6290f7
PAragon <pablo@aragon.mx>
2020-06-07 16:41:48 -0500

    Primer commit | creación del sitio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraphicsComponent } from './pages/graphics/graphics.component';
+import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children:[
+      {path: 'graphics', component: GraphicsComponent},
+      {path: 'progress', component: ProgressComponent},
+      {path: 'dashboard', component: DashboardComponent},
+      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    ]
+  },
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: PagenotfoundComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { GraphicsComponent } from './pages/graphics/graphics.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PagenotfoundComponent,
+    DashboardComponent,
+    SidebarComponent,
+    GraphicsComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
